4ea04b740df48cc46e0e9de685e1463c9d4296b8
Nicolas Boisvert <nicklay@me.com>
2024-04-19 18:44:11 -0400

    Attempt

--- bin/elm-app-cli.js
+++ bin/elm-app-cli.js
@@ -1,25 +1,26 @@
 #!/usr/bin/env node
 
-'use strict';
+"use strict";
 
-const path = require('path');
-const spawn = require('cross-spawn');
-const argv = require('minimist')(process.argv.slice(2));
-const elmExecutable = require.resolve('elm/bin/elm');
-const version = require('../package.json').version;
-const elmVersion = require('elm/package.json').version;
+const path = require("path");
+const spawn = require("cross-spawn");
+const which = require("which");
+const argv = require("minimist")(process.argv.slice(2));
+const elmExecutable = which("elm");
+const version = require("../package.json").version;
+const elmVersion = "0.19.1";
 
 const commands = argv._;
 
 const elmCommands = [
-  'repl',
-  'init',
-  'reactor',
-  'make',
-  'install',
-  'bump',
-  'diff',
-  'publish'
+  "repl",
+  "init",
+  "reactor",
+  "make",
+  "install",
+  "bump",
+  "diff",
+  "publish",
 ];
 
 if (commands.length === 0) {
@@ -30,45 +31,45 @@ if (commands.length === 0) {
 const script = commands[0];
 
 switch (script) {
-  case 'build':
-  case 'eject':
-  case 'start':
-    if (argv['help']) {
-      help(version, 'start');
+  case "build":
+  case "eject":
+  case "start":
+    if (argv["help"]) {
+      help(version, "start");
       break;
     }
 
     let args = [];
     const env = {};
-    Object.keys(argv || {}).forEach(key => {
-      if (key === 'browser') {
+    Object.keys(argv || {}).forEach((key) => {
+      if (key === "browser") {
         // `--no-browser` turns into `--browser false`
         // See https://github.com/substack/minimist/issues/123
-        args = args.concat([argv[key] ? '--browser' : '--no-browser']);
+        args = args.concat([argv[key] ? "--browser" : "--no-browser"]);
       }
-      if (key === 'debug' && argv[key] === false) {
-        env.ELM_DEBUGGER = 'false';
+      if (key === "debug" && argv[key] === false) {
+        env.ELM_DEBUGGER = "false";
       }
     });
 
-    spawnSyncNode(path.resolve(__dirname, '../scripts', script), args, env);
+    spawnSyncNode(path.resolve(__dirname, "../scripts", script), args, env);
     break;
 
-  case 'create':
-    spawnSyncNode(path.resolve(__dirname, '../scripts/create'), commands[1]);
+  case "create":
+    spawnSyncNode(path.resolve(__dirname, "../scripts/create"), commands[1]);
     break;
 
-  case 'test': {
+  case "test": {
     let args = [];
-    Object.keys(argv || {}).forEach(key => {
-      if (key !== '_' && key !== 'compiler') {
-        args = args.concat(['--' + key, argv[key]]);
+    Object.keys(argv || {}).forEach((key) => {
+      if (key !== "_" && key !== "compiler") {
+        args = args.concat(["--" + key, argv[key]]);
       }
     });
 
-    args = args.concat(['--compiler', require.resolve('elm/bin/elm')]);
-    const cp = spawn.sync(require.resolve('elm-test/bin/elm-test'), args, {
-      stdio: 'inherit'
+    args = args.concat(["--compiler", require.resolve("elm/bin/elm")]);
+    const cp = spawn.sync(require.resolve("elm-test/bin/elm-test"), args, {
+      stdio: "inherit",
     });
 
     if (cp.status !== 0) {
@@ -81,7 +82,7 @@ switch (script) {
     // Proxy elm-platform cli commands.
     if (elmCommands.indexOf(script) !== -1) {
       spawn.sync(elmExecutable, process.argv.slice(2), {
-        stdio: 'inherit'
+        stdio: "inherit",
       });
       break;
     } else {
@@ -97,34 +98,34 @@ switch (script) {
  * @param  {string} command Name of command for which to print help message
  * @return {undefined}
  */
-function help(version, command = '') {
+function help(version, command = "") {
   switch (command) {
-    case 'start':
+    case "start":
       console.log();
-      console.log('Usage: elm-app start [options...]');
+      console.log("Usage: elm-app start [options...]");
       console.log();
-      console.log('where [options...] is any number of:');
-      console.log('--no-browser\tDo not open the browser automatically');
-      console.log('--no-debug\tDisable the elm time travel debugger');
+      console.log("where [options...] is any number of:");
+      console.log("--no-browser\tDo not open the browser automatically");
+      console.log("--no-debug\tDisable the elm time travel debugger");
       console.log();
       break;
-    case 'test':
+    case "test":
       // NOTE: Current implementation calls through to `elm-test` for help message
       break;
     default:
       console.log();
-      console.log('Usage: elm-app <command>');
+      console.log("Usage: elm-app <command>");
       console.log();
-      console.log('where <command> is one of:');
-      console.log('    build, start, test, eject, ' + elmCommands.join(', '));
+      console.log("where <command> is one of:");
+      console.log("    build, start, test, eject, " + elmCommands.join(", "));
       console.log();
       console.log();
-      console.log('Elm ' + elmVersion);
+      console.log("Elm " + elmVersion);
       console.log();
   }
 
   console.log(
-    'create-elm-app@' + version + ' ' + path.resolve(__dirname, '..')
+    "create-elm-app@" + version + " " + path.resolve(__dirname, ".."),
   );
 }
 
@@ -137,9 +138,9 @@ function help(version, command = '') {
  * @return {undefined}
  */
 function spawnSyncNode(script, args, env) {
-  const cp = spawn.sync('node', [script].concat(args || []), {
-    stdio: 'inherit',
-    env: Object.assign({}, process.env, env)
+  const cp = spawn.sync("node", [script].concat(args || []), {
+    stdio: "inherit",
+    env: Object.assign({}, process.env, env),
   });
 
   if (cp.status !== 0) {
